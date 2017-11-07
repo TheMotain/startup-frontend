08ee248b2143eb488e025797f2c05a8b68ace4e7
Nesqwik <louis.guilbert@free.fr>
2017-11-07 11:36:22 +0100

    testing

--- src/utils/FormUtils.js
+++ src/utils/FormUtils.js
@@ -1,22 +1,49 @@
 // @flow
 import * as StringUtils from "./StringUtils";
 
-export function isAlpha(input: string) {
+/**
+ * check if input string is composed by A-z char and spaces only
+ * @param input string
+ * @returns true if it is, false otherwise
+ */
+export function isAlpha(input: string): boolean {
     if (typeof input !== 'string') return false;
     return /^[A-Za-z ]*$/.test(StringUtils.removeDiacritics(input));
 }
 
-export function isAlphaNum(input: any) {
+/**
+ * Check if the input is alpha numeric
+ * @param input (string or number)
+ * @returns true if it is, false otherwise
+ */
+export function isAlphaNum(input: any): boolean {
     return matchRegex(/^[a-zA-Z0-9\-_ ']*$/, input);
 }
 
-export function matchRegex(regex: any, input: any) {
+
+/**
+ * Check if input match the regex.
+ * @param regex
+ * @param input to test
+ * @returns true if it is, false otherwise
+ */
+export function matchRegex(regex: any, input: any): boolean {
     if (typeof input !== 'string' && typeof input !== 'number') return false;
     let clearedString = StringUtils.removeDiacritics(input + "");
     return regex.test(clearedString);
 }
 
-export function isEmpty(input: any) {
+/**
+ * Check if the input is empty.
+ * An empty string is "".
+ * An empty number does not exists.
+ * An empty object is {}.
+ * An empty array is [].
+ * Null or undefined is considered as empty.
+ * @param input (any type)
+ * @returns true if it is, false otherwise
+ */
+export function isEmpty(input: any): boolean {
     if (typeof input === "number") return false;
     if (typeof input === "object") {
         if (!input) return true;
@@ -25,6 +52,13 @@ export function isEmpty(input: any) {
     return !input;
 }
 
-export function isNumeric(input: any) {
+/**
+ * Check if the input is numeric.
+ * A string with value "1337" is numeric.
+ * Any number is numeric.
+ * @param input
+ * @returns true if it is, false otherwise
+ */
+export function isNumeric(input: any): boolean {
     return matchRegex(/^[0-9 ]*$/, input)
 }
