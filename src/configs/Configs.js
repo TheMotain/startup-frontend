96f6a73ff43af3da42f298272e895c9874953c9a
Nesqwik <louis.guilbert@free.fr>
2017-11-19 18:10:02 +0100

    testing some menu

--- src/configs/Configs.js
+++ src/configs/Configs.js
@@ -4,9 +4,9 @@
  */
 let ENDPOINT;
 if (process.env.NODE_ENV === 'production') {
-    ENDPOINT = "http://localhost:8080";
-} else {
     ENDPOINT = "http://149.202.179.212:8080/dev/";
+} else {
+    ENDPOINT = "http://localhost:8080/";
 }
 
 export default {
