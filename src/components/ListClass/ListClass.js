475cfe8d0e801257caf6366ec9996b47d23f05fa
Louis Guilbert <louis@energic.io>
2017-11-25 16:46:26 +0100

    some tests

--- src/components/ListClass/ListClass.js
+++ src/components/ListClass/ListClass.js
@@ -7,6 +7,12 @@ import {List} from "material-ui/List";
 import ClassItem from "./ClassItem";
 import * as ReducerUtils from "../../reducers/ReducerUtils";
 
+/**
+ * Propriétés :
+ * classes : la liste des Classroom à afficher
+ * fetchClasses : la fonction à appelet pour récupérer les classes
+ * fetchStatus : état de la requête de récupération des classes.
+ */
 type Props = {
     classes: Array<Classroom>,
     fetchClasses: () => Promise<any>,
@@ -15,13 +21,24 @@ type Props = {
 
 type State = {}
 
+/**
+ * Affiche la liste des classes.
+ */
 class ListClass extends React.Component<Props, State> {
 
+    /**
+     * Réupère la liste des classes lors de la construction du composant.
+     * @param props
+     */
     constructor(props: Props) {
         super(props);
         this.props.fetchClasses();
     }
 
+    /**
+     * Affiche la lsite des classes (renvoie un tableau de ClassItem)
+     * @returns {Array}
+     */
     renderClasses() {
         return this.props.classes.map((classroom: Classroom) => <ClassItem key={classroom.id} classroom={classroom} />);
     }
